dbf8a4f2b01531300a6df4d46deb302dcaa06f7f
MukhammadYusuf0608 <dadabeka95@gmail.com>
2024-08-09 18:22:56 +0500

    ccs ishlatim
    agavgava

---
+++ main.js
@@ -0,0 +1,44 @@
+// 1) Ikkita o'zgaruvchi yarating va ularning ichiga son saqlang va bir birini cosole.logda qo'shib chiqaring chiqaring
+
+
+// 2) let a  = '12' shu 12 sonini data typeni o'zgartirib number qilib chiqaring 
+//   bering. Yani bu hozir string typeda siz number tyga o'zgartiring
+
+
+// 3) randomli son yasang va shu randomli sonning juft yoki toq ekanligini aniqlab bering
+
+
+// 4) promptdan bitta so'z kiriting agar o'sha so'z Mars so'ziga teng bolsa o'sha so'zning
+//  o'zi chiqsin agar boshqa so'z bo'lsa Mars It school deb chiqsin
+
+
+// 5) bitta array yarating va uning ichiga 5 ta so'z kiriting. Keyin For yordamida o'sha arraydagi 
+// so'zlar ichidan uzunligi 5 va undan kattalarini bohsqa bir arrayga qo'shib chiqaring
+
+
+
+// 6) for orqali birdan 100gacha bolgan sonlarni juft yoki toqqa ajratib chiqaring
+
+
+//let num1 = 5;
+//let num2 = 10;
+//console.log(num1 + num2);
+
+
+
+
+//let str = '12';
+//str = Number(str);
+//console.log(str);
+
+
+
+let num = Math.floor(Math.random() * 100);
+ 
+if (num % 2 === 0) {
+    console.log("juft");
+    
+} else{
+    console.log('toq');
+    
+}
